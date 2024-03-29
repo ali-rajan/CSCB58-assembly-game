14ff11bf6f1c770ccc603723f1c711d36278aee4
ali-rajan <ali.rajan@mail.utoronto.ca>
2024-03-29 02:13:09 -0400

    Multiple platforms with random positioning

--- game.asm
+++ game.asm
@@ -50,13 +50,26 @@
 .eqv PLAYER_WIDTH 4
 .eqv PLAYER_HEIGHT 4
 
-.eqv PLATFORM_WIDTH 16
-.eqv PLATFORM_THICKNESS 2
+.eqv PLATFORM_WIDTH 12
+.eqv PLATFORM_THICKNESS 1
+# Bounds on the x and y-values for platforms to be randomly generated with
+.eqv PLATFORM_MIN_X 0
+.eqv PLATFORM_MAX_X 52
+.eqv PLATFORM_MIN_Y 0
+.eqv PLATFORM_MAX_Y 63
 
 # Colours
 .eqv COLOUR_BACKGROUND 0x000000     # Black
 .eqv COLOUR_PLATFORM 0x964B00       # Brown
 .eqv COLOUR_PLAYER 0xFF0000         # Red
+
+.eqv NUM_PLATFORMS 5
+
+.data
+
+# Coordinates of the platforms
+platforms_x: .word 0:NUM_PLATFORMS
+platforms_y: .word 0:NUM_PLATFORMS
 
 .text
 
@@ -67,7 +80,7 @@ j main
 
 #################### DRAWING ####################
 
-# Computes the framebuffer address of the unit (x, y)
+# Computes the framebuffer address of the unit (x, y).
 # Parameters:
     # %x: immediate unit x-value
     # %y: immediate unit y-value
@@ -92,7 +105,7 @@ j main
     add $v0, $v0, $t3       # $v0 = offset + BASE_ADDRESS
 .end_macro
 
-# Computes the framebuffer address of the unit (x, y)
+# Computes the framebuffer address of the unit (x, y).
 # Parameters:
     # %x_reg: register storing the unit's x-value
     # %y_reg: register storing the unit's y-value
@@ -115,7 +128,7 @@ j main
     add $v0, $v0, $t3       # $v0 = offset + BASE_ADDRESS
 .end_macro
 
-# Colours the given unit with the given colour
+# Colours the given unit with the given colour.
 # Parameters:
     # %addr_reg: register storing the address of the unit to colour
     # %colour: colour (immediate value)
@@ -127,12 +140,16 @@ j main
     sw $t0, 0(%addr_reg)
 .end_macro
 
-# Fills the screen with the given colour
+# Fills the screen with the given colour.
 # Parameters:
     # %colour: colour (immediate value)
 # Uses:
-#   $s0
-#   $v0: unit_address calls
+    # $t0: unit_address calls
+    # $t1: unit_address calls
+    # $t2: unit_address calls
+    # $t3: unit_address calls
+    # $v0: unit_address calls
+    # $s0
 .macro fill_background(%colour)
     unit_address(DISPLAY_RIGHT, DISPLAY_BOTTOM)
     move $s0, $v0               # $s0 = addr(last unit to colour)
@@ -147,7 +164,7 @@ _fill_background_loop:
 _fill_background_loop_end:
 .end_macro
 
-# Draws a rectangular entity with the specified attributes
+# Draws a rectangular entity with the specified attributes.
 # Parameters:
     # %x_reg: register storing the x-value of the rectangle's top-left unit
     # %y_reg: register storing the y-value of the rectangle's top-left unit
@@ -155,8 +172,11 @@ _fill_background_loop_end:
     # %height: the rectangle's height in units
     # %colour: the rectangle's colour (an immediate value)
 # Uses:
-    # %x_reg
-    # %y_reg
+    # %x_reg: unit_address_reg calls
+    # %y_reg: unit_address_reg calls
+    # $t0: colour_unit calls
+    # $t2: unit_address_reg calls
+    # $t3: unit_address_reg calls
     # $v0: unit_address_reg calls
     # $s0
     # $s1
@@ -207,10 +227,119 @@ _draw_entity_loop_end:
 .end_macro
 
 
-######################### GAME ####################
+#################### SPRITES ####################
+
+# Returns a random integer n satisfying %min <= n <= %max.
+# Parameters:
+    # %min: the range's minimum, an immediate value
+    # %max: the range's maximum, an immediate value
+# Returns:
+    # $v0: the random integer
+# Uses:
+    # $a0: random number generator syscall
+    # $a1: random number generator syscall
+    # $v0
+.macro random_integer(%min, %max)
+    li $v0, 42              # syscall code for random number generator
+    li $a0, 0               # argument for random number generator ID (any integer)
+    li $a1, %max
+    subi $a1, $a1, %min
+    addi $a1, $a1, 1        # $a1 = %max - %min + 1, the upper bound for the syscall random number
+    syscall                 # $a0 = random integer n satisfying 0 <= n < %max - %min + 1
+
+    addi $a0, $a0, %min     # $a0 is now some n satisfying %min <= n < %max + 1
+    move $v0, $a0
+.end_macro
+
+# Randomly generates x and y-values for all platforms, storing them in platform_x and platform_y (respectively).
+# Uses:
+    # $t0
+    # $t1
+    # $t2
+    # $t3
+    # $t4
+    # $t5
+    # $t6
+    # $t7
+    # $v0: random_integer call
+    # $a0: random_integer call
+    # $a1: random_integer call
+.macro initialize_platforms()
+    la $t0, platforms_x
+    la $t1, platforms_y
+    add $t2, $zero, $zero   # $t2 = array offset = sizeof(word) * i (for the index i)
+    li $t3, NUM_PLATFORMS
+    sll $t3, $t3, 2         # $t3 = NUM_PLATFORMS * sizeof(word)
+
+_initialize_platforms_loop:
+    bge $t2, $t3, _initialize_platforms_loop_end        # while i < NUM_PLATFORMS
+    add $t4, $t0, $t2
+    add $t5, $t1, $t2
+
+    # Randomly generate coordinates in the valid range and store them
+    random_integer(PLATFORM_MIN_X, PLATFORM_MAX_X)
+    move $t6, $v0
+    random_integer(PLATFORM_MIN_Y, PLATFORM_MAX_Y)
+    move $t7, $v0
+    sw $t6, 0($t4)                                  # platforms_x[i] = random x-value
+    sw $t7, 0($t5)                                  # platforms_y[i] = random y-value
+
+    addi $t2, $t2, 4
+    j _initialize_platforms_loop
+
+_initialize_platforms_loop_end:
+.end_macro
+
+# TODO: save $s0 - $s6 in stack if this is called elsewhere that uses those registers
+# Draws all the platforms based on their coordinates stored in platforms_x and platforms_y.
+# Uses:
+    # $t0: draw_entity > colour_unit calls
+    # $t2: draw_entity > unit_address_reg calls
+    # $t3: draw_entity > unit_address_reg calls
+    # $t4
+    # $t5
+    # $t6
+    # $t7
+    # $t8
+    # $t9
+    # $v0: draw_entity > unit_address_reg calls
+    # $s0: draw_entity calls
+    # $s1: draw_entity calls
+    # $s2: draw_entity calls
+    # $s3: draw_entity calls
+    # $s4: draw_entity calls
+    # $s5: draw_entity calls
+    # $s6: draw_entity calls
+.macro draw_platforms()
+    la $t8, platforms_x
+    la $t9, platforms_y
+    add $t4, $zero, $zero   # $t4 = array offset = sizeof(word) * i (for the index i)
+    li $t5, NUM_PLATFORMS
+    sll $t5, $t5, 2         # $t5 = NUM_PLATFORMS * sizeof(word)
+
+_draw_platforms_loop:
+    bge $t4, $t5, _draw_platforms_loop_end        # while i < NUM_PLATFORMS
+
+    lw $t6, 0($t8)  # $t8 = platforms_x[i]
+    lw $t7, 0($t9)  # $t9 = platforms_y[i]
+
+    draw_entity($t6, $t7, PLATFORM_WIDTH, PLATFORM_THICKNESS, COLOUR_PLATFORM)
+
+    addi $t4, $t4, 4
+    addi $t8, $t8, 4
+    addi $t9, $t9, 4
+    j _draw_platforms_loop
+
+_draw_platforms_loop_end:
+.end_macro
+
+
+#################### GAME ####################
 
 main:
     fill_background(COLOUR_BACKGROUND)
+    initialize_platforms()
+    draw_platforms()
 
     li $a0, DISPLAY_WIDTH
     subi $a0, $a0, PLAYER_WIDTH
@@ -222,16 +351,6 @@ main:
 
     draw_entity($a0, $a1, PLAYER_WIDTH, PLAYER_HEIGHT, COLOUR_PLAYER)   # Draw player
 
-    li $a0, DISPLAY_WIDTH
-    subi $a0, $a0, PLATFORM_WIDTH
-    sra $a0, $a0, 1                     # $a0 = x-value to center platform
-
-    li $a1, DISPLAY_HEIGHT
-    sra $a1, $a1, 1
-    addi $a1, $a1, PLATFORM_THICKNESS   # $a1 = y-value to center platform below player
-
-    draw_entity($a0, $a1, PLATFORM_WIDTH, PLATFORM_THICKNESS, COLOUR_PLATFORM)     # Draw platform
-
     # Exit
-    li      $v0,    10
+    li $v0, 10
     syscall
